ca8717ce0929b3e6c12a046aee2dcdb521faa44c
cnpittsford <136652182+cnpittsford@users.noreply.github.com>
2025-12-04 17:28:00 -0500

    12/4/25

--- Assets/Scripts/CapsuleShooting.cs
+++ Assets/Scripts/CapsuleShooting.cs
@@ -12,17 +12,15 @@ public class CapsuleShooting : MonoBehaviour
     private float shotTimer;
 
     void Update() {
-        if(capsuleMovement.stopped) {
-            shotTimer += Time.deltaTime;
-            if(shotTimer >= 2) {
-                GameObject shot = Instantiate(laser, gameObject.transform.position, transform.rotation);
-                shot.transform.Rotate(0, 0, 90);
-                SpriteRenderer renderer = shot.GetComponent<SpriteRenderer>();
-                renderer.sortingOrder = -5;
-                shot.tag = "CapsuleShot";
-                shot.SetActive(true);
-                shotTimer = 0;
-            }
+        shotTimer += Time.deltaTime;
+        if(shotTimer >= 2) {
+            GameObject shot = Instantiate(laser, gameObject.transform.position, transform.rotation);
+            shot.transform.Rotate(0, 0, 90);
+            SpriteRenderer renderer = shot.GetComponent<SpriteRenderer>();
+            renderer.sortingOrder = -5;
+            shot.tag = "CapsuleShot";
+            shot.SetActive(true);
+            shotTimer = 0;
         }
     }
 }
